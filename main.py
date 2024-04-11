eb1ca711e5161992fbf9aaa48a653f8ad915e6f0
mvpdvlp <mvpdvlp@gmail.com>
2024-04-10 22:12:17 -0400

    Add game window configuration

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import pygame
 import random
 import math
+
+# Constant import
+import constant as SET_UP 
+
+# init
+pygame.init()
+
+# Game configurations
+WINDOW = pygame.display.set_mode((SET_UP.WIDTH,SET_UP.HEIGHT)) # display configuration
+pygame.display.set_caption("2048") # Set title for game window
+
+'''
+    Game font configuration
+    init custom font
+    name : Winter Pixel 24 BOLD
+    from: https://www.fontspace.com/category/pixel
+'''
+# font_path = "/font/StWinterPixel24BoldDemoBold-BWRz5.otf"
+# font_size = 24
+# custom_font = pygame.font.Font(font_path, font_size)
+
+FONT = pygame.font.SysFont("Roboto",60,bold=False)
