85c55cd8388ff1a09c4c0ef51dd86876ef3f9613
mvpdvlp <mvpdvlp@gmail.com>
2024-04-10 21:51:08 -0400

    Adding project constant values

---
+++ constant.py
@@ -0,0 +1,23 @@
+# Constant definition
+
+FPS = 60 # frames per second
+
+# Display configuration
+WIDTH, HEIGHT = 800,800 # Window size
+
+# Layaout configuration game
+ROW=4
+COLS=4
+
+# Rect size inside grid
+RECT_HEIGHT = HEIGHT // ROW
+RECT_WIDTH = WIDTH // COLS
+
+# Color define
+OUTLINE_COLOR = (187,173,160) # using RGV
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192,180)
+FONT_COLOR= (119,110,101)
+
+# Pygame Window
+ 
