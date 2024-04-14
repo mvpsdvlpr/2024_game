2612f54a1ffc5cae583307c6c853a6b13b16e47a
mvpdvlp <mvpdvlp@gmail.com>
2024-04-14 11:46:30 -0400

    Separate values on file

--- constant.py
+++ constant.py
@@ -1,20 +1,19 @@
 # Constant definition
-
 FPS = 60 # frames per second
 
 # Display configuration
 WIDTH, HEIGHT = 800,800 # Window size
 
-# Layaout configuration game
-ROW=4
+# grid configuration game
+ROWS=4
 COLS=4
 
 # Rect size inside grid
-RECT_HEIGHT = HEIGHT // ROW
+RECT_HEIGHT = HEIGHT // ROWS
 RECT_WIDTH = WIDTH // COLS
 
 # Color define
-OUTLINE_COLOR = (187,173,160) # using RGV
+OUTLINE_COLOR = (0,0,0) # using RGB (187,173,160)
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205, 192,180)
 FONT_COLOR= (119,110,101)
